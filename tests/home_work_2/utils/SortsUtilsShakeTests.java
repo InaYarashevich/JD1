ada8d23e1b12ee83336307407dc5913168dc6912
InaYarashevich <ina.yarashevich@gmail.com>
2022-09-20 18:19:48 +0300

    adding tests to Task 4 - SortsUtilsShake

--- tests/home_work_2/utils/SortsUtilsShakeTests.java
+++ tests/home_work_2/utils/SortsUtilsShakeTests.java
@@ -1,4 +1,13 @@
 package home_work_2.utils;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class SortsUtilsShakeTests {
+    @Test
+    public void bubbleSort1Test() {
+        Assertions.assertArrayEquals(
+                new int[]{6, 5, 4, 3, 2, 1},
+                SortsUtils.shake(new int[]{1, 2, 3, 4, 5, 6}));
+    }
 }
