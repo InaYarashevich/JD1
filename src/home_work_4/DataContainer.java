202e0b2540a059b966ae77f98ce1d617e5bf7f7d
InaYarashevich <ina.yarashevich@gmail.com>
2022-09-21 15:18:25 +0300

    refactoring

--- src/home_work_4/DataContainer.java
+++ src/home_work_4/DataContainer.java
@@ -5,8 +5,9 @@ import home_work_4.comparators.ComparatorComparable;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Iterator;
+import java.util.Objects;
 
-public class DataContainer<T> implements Iterable<T>{
+public class DataContainer<T> implements Iterable<T> {
 
     private T[] data;
 
@@ -15,70 +16,85 @@ public class DataContainer<T> implements Iterable<T>{
     }
 
 
-   /* public int add(T item) {
+    public int add(T item) {
         if (item == null) {
             return -1;
         }
+
         int indexForInsert = -1;
         for (int i = 0; i < this.data.length; i++) {
-            if (this.data[i] == null){
+            if (this.data[i] == null) {
                 indexForInsert = i;
                 break;
             }
         }
-    }*/
+
+        if (indexForInsert == -1) {
+            indexForInsert = this.data.length;
+            this.data = Arrays.copyOf(this.data, this.data.length + 1);
+        }
+
+        this.data[indexForInsert] = item;
+
+        return indexForInsert;
+    }
 
     public T get(int index) {
-        return data[index];
+        if (!checkIndexExist(index)) {
+            return null;
+        }
+        return this.data[index];
     }
+
 
     public T[] getItems() {
         return data;
     }
 
+    private boolean checkIndexExist(int index) {
+        return index >= 0 && index < this.data.length;
+    }
+
+
     public boolean delete(int index) {
-        T[] newArray = Arrays.copyOf(data, data.length);
-        if ((index < 0) || (index >= data.length)) {
+        if (!checkIndexExist(index)) {
             return false;
         }
-        int j = 0;
-        for (int i = 0; i < data.length; i++) {
-            if (i == index) {
-                data[i] = null;
-            }
-            newArray[j++] = data[i];
+
+        for (int i = index + 1; i < this.data.length; i++) {
+            this.data[i - 1] = this.data[i];
         }
+        this.data = Arrays.copyOf(this.data, this.data.length - 1);
         return true;
     }
 
     public boolean delete(T item) {
-        T[] newArray = null;
-        for (int i = 0; i < data.length; i++) {
-            if (data[i].equals(item)) {
-                T first = data[0];
-                data[i] = null;
-                data[i] = data[i + 1];
-                newArray = Arrays.copyOf(data, data.length - 1);
-                return true;
+        if (item == null) {
+            return false;
+        }
+
+        for (int i = 0; i < this.data.length; i++) {
+            if (Objects.equals(this.data[i], item)) {
+                return delete(i);
             }
         }
         return false;
     }
 
-    public void sort(Comparator<T> cmp){
+    public void sort(Comparator<T> cmp) {
         DataContainer.sort(this, cmp);
     }
 
-    public static void sort(DataContainer<? extends Comparable> container){
+    public static void sort(DataContainer<? extends Comparable> container) {
         DataContainer.sort(container, new ComparatorComparable());
     }
 
-    public static <T> void sort(DataContainer<T> container, Comparator<? super T> cmp){
+    public static <T> void sort(DataContainer<T> container, Comparator<? super T> cmp) {
         boolean sorted;
         do {
             sorted = true;
-            for (int j = 0; j < container.data.length-1; j++){
-                if(cmp.compare(container.data[j], container.data[j + 1]) > 0){
+            for (int j = 0; j < container.data.length - 1; j++) {
+                if (cmp.compare(container.data[j], container.data[j + 1]) > 0) {
                     sorted = false;
                     T tmp = container.data[j + 1];
                     container.data[j + 1] = container.data[j];
@@ -88,13 +104,44 @@ public class DataContainer<T> implements Iterable<T>{
         } while (!sorted);
     }
 
-    @Override
-    public String toString() {
-        return Arrays.toString(data);
+    public Iterator<T> iterator() {
+        return new Iter();
     }
 
-    @Override
-    public Iterator<T> iterator() {
-        return null;
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("[");
+        boolean needComma = false;
+        for (T datum : this.data) {
+            if (datum != null) {
+                if (needComma) {
+                    builder.append(", ");
+                } else {
+                    needComma = true;
+                }
+                builder.append(datum);
+            }
+        }
+        builder.append("]");
+        return builder.toString();
+    }
+
+    private class Iter implements Iterator<T> {
+        private int currentPosition = -1;
+
+        @Override
+        public boolean hasNext() {
+            return checkIndexExist(currentPosition + 1);
+        }
+
+        @Override
+        public T next() {
+            return data[++currentPosition];
+        }
+
+        @Override
+        public void remove() {
+            delete(currentPosition);
+        }
     }
 }
