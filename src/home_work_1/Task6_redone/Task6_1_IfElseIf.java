75a134d0e013c9cb79278f8a123198aa8c77d47c
InaYarashevich <ina.yarashevich@gmail.com>
2022-09-26 10:41:13 +0300

    redoing Task 6 - if-else-if case

---
+++ src/home_work_1/Task6_redone/Task6_1_IfElseIf.java
@@ -0,0 +1,23 @@
+package home_work_1.Task6_redone;
+
+import home_work_1.Task6_redone.api.ICommunicationPrinter;
+
+public class Task6_1_IfElseIf implements ICommunicationPrinter {
+
+    @Override
+    public String welcome(String name) {
+        try {
+            if (name.equalsIgnoreCase("Вася")) {
+                return "Привет! \nЯ тебя так долго ждал";
+            } else {
+                if (name.equalsIgnoreCase("Анастасия")) {
+                    return "Я тебя так долго ждал";
+                } else {
+                    return "Добрый день, а вы кто?";
+                }
+            }
+        } catch (NullPointerException e){
+            return "Добрый день, а вы кто?";
+        }
+    }
+}
