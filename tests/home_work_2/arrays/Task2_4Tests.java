c848ae260d2759c144d30a952437e8e752d4bf86
InaYarashevich <ina.yarashevich@gmail.com>
2022-09-26 16:43:08 +0300

    refactoring tests

--- tests/home_work_2/arrays/Task2_4Tests.java
+++ tests/home_work_2/arrays/Task2_4Tests.java
@@ -1,26 +1,41 @@
 package home_work_2.arrays;
 
-import home_work_2.arrays.Task2_4;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+        import home_work_2.arrays.Task2_4;
+        import org.junit.jupiter.api.Assertions;
+        import org.junit.jupiter.api.Test;
 
-public class Task2_4Test {
+public class Task2_4Tests {
     @Test
-    public void getSumOfEvenPositiveNumbers1Test(){
+    public void getSumOfEvenPositiveNumbers1Test() {
         Assertions.assertTrue(
                 (Task2_4.getSumOfEvenPositiveNumbers(50, 100) >= 0)
                         && (Task2_4.getSumOfEvenPositiveNumbers(50, 100) % 2 == 0));
     }
 
     @Test
-    public void getSumOfEvenPositiveNumbers2Test(){
+    public void getSumOfEvenPositiveNumbers2Test() {
         Assertions.assertTrue(
                 (Task2_4.getSumOfEvenPositiveNumbers(5, 100) >= 0)
                         && (Task2_4.getSumOfEvenPositiveNumbers(5, 100) % 2 == 0));
     }
 
     @Test
-    public void getMaxTest(){
-        //Assertions.assertTrue();
+    public void getMaxTest() {
+        Assertions.assertNotNull(Task2_4.getMax(10, 100));
+    }
+
+    @Test
+    public void getElementsLessThanArithmeticMeanTest(){
+        Assertions.assertNotNull(Task2_4.getElementsLessThanArithmeticMean(5, 100).length);
+    }
+
+    @Test
+    public void getSumOfArrayElementsNumbersTest(){
+        Assertions.assertNotNull(Task2_4.getSumOfArrayElementsNumbers(5, 200));
+    }
+
+    @Test
+    public void removeElementsFromArray(){
+        Assertions.assertArrayEquals(new int[]{}, Task2_4.removeElementsFromArray(5, 100));
     }
 }
