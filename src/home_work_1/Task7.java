fa1bfb8d3a61858a9744d85ff53b1a98bf4700b4
InaYarashevich <ina.yarashevich@gmail.com>
2022-09-26 09:52:47 +0300

    completed Task 7

--- src/home_work_1/Task7.java
+++ src/home_work_1/Task7.java
@@ -2,11 +2,22 @@ package home_work_1;
 
 public class Task7 {
     public static void main(String[] args) {
-
+        int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        System.out.println(createPhoneNumber(array));
     }
 
-    public static String createPhoneNumber(int[] array){
-        String phoneNumber = "/^\\(?([0-9]{3})\\)?[-.\\s]?([0-9]{3})[-.\\s]?([0-9]{4})$/";
+    public static String createPhoneNumber(int[] numbers) {
+        String phoneNumber = "";
+        for (int i = 0; i < 10; i++) {
+            if (i == 0) {
+                phoneNumber = phoneNumber.concat("(");
+            } else if (i == 3) {
+                phoneNumber = phoneNumber.concat(") ");
+            } else if (i == 6) {
+                phoneNumber = phoneNumber.concat("-");
+            }
+            phoneNumber = phoneNumber.concat(String.valueOf(numbers[i]));
+        }
         return phoneNumber;
     }
 }
