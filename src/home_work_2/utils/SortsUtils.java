066fea4ec19083b010bfb8d7b54b3bc2e5439fd6
InaYarashevich <ina.yarashevich@gmail.com>
2022-09-27 14:33:31 +0300

    refactoring shaker sorting algorythm, tests

--- src/home_work_2/utils/SortsUtils.java
+++ src/home_work_2/utils/SortsUtils.java
@@ -1,40 +1,40 @@
 package home_work_2.utils;
 
 public class SortsUtils {
-    public static int[] bubble(int[] arr) {
-        int temp = 0;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 1; j < (arr.length - i); j++) {
-                if (arr[j - 1] > arr[j]) {
-                    temp = arr[j - 1];
-                    arr[j - 1] = arr[j];
-                    arr[j] = temp;
+    public static int[] bubble(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = (array.length - 1); j > i; j--) {
+                if (array[j - 1] > array[j]) {
+                    int temp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = temp;
                 }
             }
         }
-        return arr;
+        return array;
     }
 
     public static int[] shake(int[] arr) {
         for (int i = 0; i < arr.length/2; i++) {
             boolean swapped = false;
             for (int j = i; j < arr.length - i - 1; j++) {
-
-                if (arr[j] < arr[j+1]) {
+                if (arr[j+1] < arr[j]) {
                     int tmp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = tmp;
                     swapped = true;
                 }
             }
+
             for (int j = arr.length - 2 - i; j > i; j--) {
-                if (arr[j] > arr[j-1]) {
+                if (arr[j-1] > arr[j]) {
                     int tmp = arr[j];
                     arr[j] = arr[j-1];
                     arr[j-1] = tmp;
                     swapped = true;
                 }
             }
+
             if(!swapped) break;
         }
         return arr;
--- tests/home_work_2/utils/SortsUtilsShakeTests.java
+++ tests/home_work_2/utils/SortsUtilsShakeTests.java
@@ -3,11 +3,89 @@ package home_work_2.utils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.logging.Logger;
+
 public class SortsUtilsShakeTests {
+    public static final Logger LOGGER =
+            Logger.getLogger(SortsUtilsBubbleTests.class.getName());
+
     @Test
-    public void bubbleSort1Test() {
+    public void shakeSort1Test() {
+        int[] array = new int[]{1, 2, 3, 4, 5, 6};
+        LOGGER.info(Arrays.toString(array) + " -> " + Arrays.toString(SortsUtils.shake(array)));
         Assertions.assertArrayEquals(
-                new int[]{6, 5, 4, 3, 2, 1},
-                SortsUtils.shake(new int[]{1, 2, 3, 4, 5, 6}));
+                new int[]{1, 2, 3, 4, 5, 6},
+                SortsUtils.shake(array));
     }
+
+    @Test
+    public void shakeSort2Test() {
+        int[] array = new int[]{1, 1, 1, 1};
+        LOGGER.info(Arrays.toString(array) + " -> " + Arrays.toString(SortsUtils.shake(array)));
+        Assertions.assertArrayEquals(
+                array,
+                SortsUtils.shake(array));
+    }
+
+    @Test
+    public void shakeSort3Test() {
+        int[] array = new int[]{9, 1, 5, 99, 9, 9};
+        LOGGER.info(Arrays.toString(array) + " -> " + Arrays.toString(SortsUtils.shake(array)));
+        Assertions.assertArrayEquals(
+                new int[]{1, 5, 9, 9, 9, 99},
+                SortsUtils.shake(array));
+    }
+
+    @Test
+    public void shakeSort5Test() {
+        int[] array = new int[]{};
+        LOGGER.info(Arrays.toString(array) + " -> " + Arrays.toString(SortsUtils.shake(array)));
+        Assertions.assertArrayEquals(
+                new int[]{},
+                SortsUtils.shake(array));
+    }
+
+    @Test
+    public void shakeSort6Test() {
+        int[] array = new int[]{6, 5, 4, 3, 2, 1};
+        LOGGER.info(Arrays.toString(array) + " -> " + Arrays.toString(SortsUtils.shake(array)));
+        Assertions.assertArrayEquals(
+                new int[]{1, 2, 3, 4, 5, 6},
+                SortsUtils.shake(array));
+    }
+
+    @Test
+    public void shakeSort7Test() {
+        int[] array = new int[]{-2, 31, 0, 15, 16, 15};
+        LOGGER.info(Arrays.toString(array) + " -> " + Arrays.toString(SortsUtils.shake(array)));
+        Assertions.assertArrayEquals(
+                new int[]{-2, 0, 15, 15, 16, 31},
+                SortsUtils.shake(array));
+    }
+
+    @Test
+    public void shakeSort8Test() {
+        int[] array = new int[]{3, 0, 1, 0, 2};
+        LOGGER.info(Arrays.toString(array) + " -> " + Arrays.toString(SortsUtils.shake(array)));
+        Assertions.assertArrayEquals(
+                new int[]{0, 0, 1, 2, 3},
+                SortsUtils.shake(array));
+    }
+
+    /* @Test
+    public void shakeSort9Test() {
+        int[] array = ArraysUtils.arrayRandom(50, 100);
+        int[] sortedArray = new int[array.length];
+        Assertions.assertArrayEquals(
+                sortedArray,
+                SortsUtils.shake(array));
+    }
+
+    @Test
+    public void shakeSort10Test() {
+        Assertions.assertArrayEquals(
+                new int[]{11, 13, 16, 24, 35, 36},
+                SortsUtils.shake(ArraysUtils.arrayFromConsole()));
+    }*/
 }
