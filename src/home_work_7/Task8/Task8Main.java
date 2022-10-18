3eb334992403fb0fffa1f8c187732628b7010b82
InaYarashevich <ina.yarashevich@gmail.com>
2022-10-18 16:49:35 +0300

    refactoring

--- src/home_work_7/Task8/Task8Main.java
+++ src/home_work_7/Task8/Task8Main.java
@@ -12,29 +12,42 @@ import java.util.concurrent.*;
 
 public class Task8Main {
     public static void main(String[] args) throws InterruptedException, ExecutionException {
-        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
         EasySearch easySearch = new EasySearch();
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите адрес папки: ");
         String path = scanner.next();
 
-        System.out.println("Введите искомое слово: ");
-        String word = scanner.next();
 
         List<String> files = ScanFolder.getFiles(path);
         List<String> texts = new ArrayList<>();
-        for (int i = 0; i < files.size(); i++) {
-            int iter = i;
-            texts.add(ReadFile.read(path + "\\" + files.get(i)));
-            Callable<Long> callableTask = () -> easySearch.search(texts.get(iter), word);
-            List<Callable<Long>> callables = new ArrayList<>();
-            for (int j = 0; j < files.size(); j++) {
-                callables.add(callableTask);
+        int choice = 0;
+
+        do {
+            System.out.println("Введите искомое слово: ");
+            String word = scanner.next();
+
+            for (int i = 0; i < files.size(); i++) {
+                int iter = i;
+                texts.add(ReadFile.read(path + "\\" + files.get(i)));
+
+                Callable<Long> callableTask = () -> easySearch.search(texts.get(iter), word);
+
+                List<Callable<Long>> callables = new ArrayList<>();
+
+                for (int j = 0; j < files.size(); j++) {
+                    callables.add(callableTask);
+                }
+
+                List<Future<Long>> futures = executorService.invokeAll(callables);
+
+                WriteIntoFile.writeIntoFile(files.get(i) + " - " + word + " - " + futures.get(i).get(), "result.txt");
             }
-            List<Future<Long>> futures = executorService.invokeAll(callables);
-            WriteIntoFile.writeIntoFile(files.get(i) + " - " + word + " - " + futures.get(i).get(), "result.txt");
-        }
+            System.out.println("0 - продолжить поиск в текущем файле;\n1 - завершить поиск в текущем файле;");
+            choice = scanner.nextInt();
+        } while (choice != 1);
+
         executorService.shutdown();
     }
 }
