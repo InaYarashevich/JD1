5c8c066f1da3e4a39c0c9e6b02781a875e1f6534
InaYarashevich <ina.yarashevich@gmail.com>
2022-10-21 21:25:43 +0300

    refactoring

--- src/home_work_7/task2/Task2_2.java
+++ src/home_work_7/task2/Task2_2.java
@@ -5,7 +5,15 @@ import home_work_7.comparator.ComparatorInteger;
 import java.util.*;
 
 public class Task2_2 {
-    public static Map<String, Integer> createMapFromString(String text) {
+    
+    /**
+     * метод создает Map из переданного как аргумент текста, где ключ - слово,
+     * значение - количество раз, которое слово встречается в тексте
+     *
+     * @param text - текст, из которого будет создаваться Map
+     * @return Map, состояющую из ключей (слово) и значений (количество раз слово встречается в тексте)
+     */
+    public Map<String, Integer> createMapFromString(String text) {
         String[] words = text.split("[, ;.!]+");
         Map<String, Integer> wordsMap = new HashMap<>();
 
@@ -24,7 +32,13 @@ public class Task2_2 {
         return wordsMap;
     }
 
-    public static List<Integer> createListFromMapValues(Map<String, Integer> wordsMap) {
+    /**
+     * метод создает список из значений Map, переданной как аргумент
+     *
+     * @param wordsMap - Map, из значений которой будет создаваться список
+     * @return список значений (количество раз, которые слова встречаются в тексте)
+     */
+    public List<Integer> createListFromMapValues(Map<String, Integer> wordsMap) {
         List<Integer> valuesList = new ArrayList<>();
         for (Map.Entry<String, Integer> entry : wordsMap.entrySet()) {
             valuesList.add(entry.getValue());
@@ -32,18 +46,34 @@ public class Task2_2 {
         return valuesList;
     }
 
-    public static List<Integer> sortListInDescendingOrder(List<Integer> valuesList) {
+    /**
+     * метод сортирует список int значений в убывающем порядке
+     *
+     * @param valuesList список значений, который нужно отсортировать
+     * @return отсортированный список
+     */
+    public List<Integer> sortListInDescendingOrder(List<Integer> valuesList) {
         ComparatorInteger comparator = new ComparatorInteger();
         List<Integer> listToSort = new ArrayList<>(valuesList);
         Collections.sort(listToSort, Collections.reverseOrder());
         return listToSort;
     }
 
-    public static Map<String, Integer> getTopWords(Map<String, Integer> wordsMap, List<Integer> valuesList, int topCount){
+    /**
+     * метод выбирает из Map слова, которые чаще всего встречаются в тексте, по которому был произведен поиск
+     *
+     * @param wordsMap   - Map, где ключ - строка (слово), поиск которой был осуществлен в тексте,
+     *                   значение - количество раз, слово встретилось в тексте
+     * @param valuesList - отсортированный в порядке убывания список значений, отражающий количество раз,
+     *                   которое слово встречается в тексте
+     * @param topCount   - количество результатов, которые нужно вернуть
+     * @return Map слов, которые в тексте встречаются чаще всего
+     */
+    public Map<String, Integer> getTopWords(Map<String, Integer> wordsMap, List<Integer> valuesList, int topCount) {
         Map<String, Integer> topResults = new HashMap<>();
         for (int i = 0; i < valuesList.subList(0, topCount).size(); i++) {
             for (Map.Entry<String, Integer> entry : wordsMap.entrySet()) {
-                if (entry.getValue().equals(valuesList.subList(0, topCount).get(i))){
+                if (entry.getValue().equals(valuesList.subList(0, topCount).get(i))) {
                     topResults.put(entry.getKey(), valuesList.subList(0, topCount).get(i));
                 }
             }
