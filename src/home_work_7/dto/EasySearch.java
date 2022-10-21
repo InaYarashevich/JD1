e1b81716a2f87ae90469f42f1cf0f984a639a733
InaYarashevich <ina.yarashevich@gmail.com>
2022-10-21 19:58:33 +0300

    refactoring

--- src/home_work_7/dto/EasySearch.java
+++ src/home_work_7/dto/EasySearch.java
@@ -3,11 +3,12 @@ package home_work_7.dto;
 import home_work_7.api.ISearchEngine;
 
 public class EasySearch implements ISearchEngine {
-    private static final String[]FIRST = {" ",".","?",",","!", "..", "...", ";", ":", " -", "--", "*", "'", "\"", "(", ")"};
-    private static final String[]LAST = {" ","\n",".","?",",","!", "..", "...", ";", ":", "--", "*", "'", "\"", "(", ")"};
+    private static final String[] FIRST = {" ", ".", "?", ",", "!", "..", "...", ";", ":", " -", "--", "*", "'", "\"", "(", ")"};
+    private static final String[] LAST = {" ", "\n", ".", "?", ",", "!", "..", "...", ";", ":", "--", "*", "'", "\"", "(", ")"};
 
     /**
      * метод поиска слова в тексте
+     *
      * @param text = текст, в котором будет осуществляться поиск
      * @param word = искомое слово
      * @return = количество раз, которое искомое слово встречается в тексте
@@ -17,28 +18,28 @@ public class EasySearch implements ISearchEngine {
         int index = 0;
         long count = 0;
 
-        if(text.startsWith(word) && text.endsWith(word) && (word.length() == text.length())){
+        if (text.startsWith(word) && text.endsWith(word) && (word.length() == text.length())) {
             count++;
         }
-        for (int i = 0; i < LAST.length; i++){
-            if(text.indexOf((word + LAST[i]), (index)) == 0){
+        for (int i = 0; i < LAST.length; i++) {
+            if (text.indexOf((word + LAST[i]), (index)) == 0) {
                 count++;
             }
-            for (int j = 0; j < FIRST.length; j++){
+            for (int j = 0; j < FIRST.length; j++) {
                 boolean flag = true;
-                while (flag){
-                    if((index = text.indexOf((FIRST[j] + word + LAST[i]), (index + 1))) != 0 &&
-                            text.indexOf((FIRST[j] + word + LAST[i]), (index + 1)) != -1){
+                while (flag) {
+                    if ((index = text.indexOf((FIRST[j] + word + LAST[i]), (index + 1))) != 0 &&
+                            text.indexOf((FIRST[j] + word + LAST[i]), (index + 1)) != -1) {
                         count++;
                     }
-                    if (index == -1){
+                    if (index == -1) {
                         flag = false;
                     }
                 }
             }
         }
-        for (int j = 0; j < FIRST.length; j++){
-            if(text.endsWith(FIRST[j] + word)){
+        for (int j = 0; j < FIRST.length; j++) {
+            if (text.endsWith(FIRST[j] + word)) {
                 count++;
             }
         }
@@ -47,12 +48,13 @@ public class EasySearch implements ISearchEngine {
 
     /**
      * метод возвращает результат поиска слова в тексте книги
-     * @param bookName - название книги, в которой осществлен поиск
+     *
+     * @param bookName   - название книги, в которой осществлен поиск
      * @param searchItem - слово, по которому был произведен поиск
      * @param foundCount - количество раз, которое искомое слово встретилось в тексте книги
      * @return - результат поиска, отражающий название книги, искомое слово и количество совпадений, разделенных тире
      */
-    public String getSearchResult(String bookName, String searchItem, long foundCount){
+    public String getSearchResult(String bookName, String searchItem, long foundCount) {
         return bookName + " - " + searchItem + " - " + foundCount;
     }
 }
--- src/home_work_7/dto/RegExSearch.java
+++ src/home_work_7/dto/RegExSearch.java
@@ -8,19 +8,26 @@ import java.util.regex.Pattern;
 public class RegExSearch implements ISearchEngine {
     private static final String REGEX = "[а-я]+";
 
+    /**
+     * метод поиска слова в тексте
+     *
+     * @param text = текст, в котором будет осуществляться поиск
+     * @param word = искомое слово
+     * @return = количество раз, которое искомое слово встречается в тексте
+     */
     @Override
     public long search(String text, String word) {
         long count = 0;
 
         Pattern first = Pattern.compile("^" + word + "$");
         Matcher fir = first.matcher(text);
-        if(fir.matches()){
+        if (fir.matches()) {
             count++;
         }
 
         first = Pattern.compile("(?m)^" + word + "\\s*(\\s-|\\s|--|!|\"|;|'|:|\\(|\\)|\\*|,|\\?|\\.)\\s*");
         fir = first.matcher(text);
-        if(fir.find(0)){
+        if (fir.find(0)) {
             count++;
         }
 
@@ -30,16 +37,16 @@ public class RegExSearch implements ISearchEngine {
         Matcher matcher = patern.matcher(text);
 
         boolean flag;
-        while ((flag = matcher.find())){
-            if(flag){
+        while ((flag = matcher.find())) {
+            if (flag) {
                 count++;
             }
         }
 
-        patern =  Pattern.compile("\\s*(\\s-|\\s|--|!|\"|;|'|:|\\(|\\)|\\*|,|\\?|\\.)\\s*"
+        patern = Pattern.compile("\\s*(\\s-|\\s|--|!|\"|;|'|:|\\(|\\)|\\*|,|\\?|\\.)\\s*"
                 + word + "$");
         matcher = patern.matcher(text);
-        if(matcher.find()){
+        if (matcher.find()) {
             count++;
         }
 
